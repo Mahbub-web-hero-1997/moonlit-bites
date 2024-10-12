2c838d1766eb5afe7c7fdcf4b76544b44965256f
Mahbub-web-hero-1997 <mahbub.programing.hero@gmail.com>
2024-10-12 23:33:22 +0600

    seme error handle

--- src/Components/Shared/Category.jsx
+++ src/Components/Shared/Category.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import UseCategory from '../../CustomHook/UseCategory';
-import { ActiveLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Category = () => {
     const categories = UseCategory() 
@@ -17,9 +17,9 @@ const Category = () => {
             {
                 categories.map((category) => (<>
                     <div >                      
-                            <ActiveLink to="#" className=' flex flex-col justify-center items-center gap-x-10  hover:text-orange-500 text-xl font-semibold'><img className=' w-28 h-28 rounded-full hover:scale-105 ' src={category.image} alt="" />
+                            < Link to="#" className=' flex flex-col justify-center items-center gap-x-10  hover:text-orange-500 text-xl font-semibold'><img className=' w-28 h-28 rounded-full hover:scale-105 ' src={category.image} alt="" />
                             <h1 className=''>{category.Name}</h1>
-                            </ActiveLink>                     
+                            </Link>                     
                 </div>
                 </>))
             }

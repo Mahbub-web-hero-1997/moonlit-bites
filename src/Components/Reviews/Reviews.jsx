e7965a76e5381526f16d5e71626ef289ae02346a
Mahbub-web-hero-1997 <mahbub.programing.hero@gmail.com>
2024-12-10 19:03:22 +0600

    some changes on review component

--- src/Components/Reviews/Reviews.jsx
+++ src/Components/Reviews/Reviews.jsx
@@ -1,8 +1,9 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import SectionHeading from '../Shared/SectionHeading';
+import Review from './Review';
 
-const Review = () => {
+const Reviews = () => {
     const [reviews, setReviews] = useState([])
     useEffect(() => {
         const url="http://localhost:5000/review"
@@ -13,11 +14,13 @@ const Review = () => {
     return (
         <>
             <SectionHeading heading={"Testimonials"} subHeading={"Reviews"}/>
-            <div>
-                
+            <div className='w-full grid grid-cols-1 md:grid-cols-3 gap-3 md:gap-0'>
+                {
+                    reviews.map(review => <Review review={ review} key={review._id} />)
+               } 
         </div>
         </>
     );
 };
 
-export default Review;
+export default Reviews;
